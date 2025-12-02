3a02f92d90b1a56004113bde5e917076ca9e8418
zermatox <jvztiin@gmail.com>
2025-12-01 17:38:18 -0800

    Added main menu + tree interaction mechanic
    Check discord for full update

---
+++ Assets/HoldToInteract.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HoldToInteract : MonoBehaviour
+{
+    public float holdTime = 6f; // Amount of time needed for the player to hold E to plant the presents by the tree
+    public GameObject uiObject; // UI Panel "PlantUI" to pop up as a prompt on screen
+    public Slider progressSlider; // Progress bar to indicate how long we've held E for
+
+    private bool playerInside = false; // Initially false
+    private float holdProgress = 0f; // Initially 0
+    private bool canPlant = true; // Initially possible to plant a present under the tree
+
+    void Start()
+    {
+        uiObject.SetActive(false); // Initially hidden
+        progressSlider.value = 0f; // Initially 0 for slider
+    }
+
+    void Update()
+    {
+        // If the tree's sphere collider detects a player inside
+        if (playerInside && canPlant)
+        {
+            // E is the input needed for progress to be made
+            if (Input.GetKey(KeyCode.E))
+            {
+                // holdProgress holds the point in which we started holding E
+                holdProgress += Time.deltaTime;
+                // Slider reflects the amount of time we've been holding E since we first held it
+                progressSlider.value = holdProgress / holdTime;
+
+                // If we've held it as long as needed, plant successful
+                if (holdProgress >= holdTime)
+                {
+                    CompleteInteraction();
+                }
+            }
+            else
+            {
+                // If let go, progress is interrupted and is decreasing over time
+                holdProgress -= Time.deltaTime * 2f;
+                holdProgress = Mathf.Max(0f, holdProgress);
+                progressSlider.value = holdProgress / holdTime;
+            }
+        }
+    }
+
+    private void CompleteInteraction()
+    {
+        Debug.Log("Interaction complete!");
+        canPlant = false; // We can no longer plant more presents under this tree
+        uiObject.SetActive(false); // Hide UI panel now
+        playerInside = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Look for object with tag "Player", should only be player object that has this
+        if (other.CompareTag("Player") && canPlant)
+        {
+            playerInside = true; // If player found, set this to true
+            uiObject.SetActive(true); // Show UI panel
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // When the player leaves the trigger collider, then hide the UI and player is no longer inside
+        if (other.CompareTag("Player") && canPlant)
+        {
+            playerInside = false;
+            holdProgress = 0f;
+            progressSlider.value = 0f;
+            uiObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/MainMenu.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private float delayTime = 1f;       // Default delay time
+    public GameObject QuitYesOrNoUI;    // A prompt that asks if player is sure to quit the game
+    public GameObject MainMenuUI;       // Main Menu UI
+    public GameObject optionsMenu;      // Options menu for enabling/disabling
+    public GameObject UIBlocker;        // Panel that is raycastable so it blocks button from being able to be pressed
+    public FadeIn fadeTransition;       // NEED TO ADD
+    public MenuMusic menuMusic;         // NEED TO ADD
+
+    public void PlayGame()
+    {
+        // When player clicks PLAY, head into next scene with a slight delay, allowing for fade transition and music to fade
+        StartCoroutine(DelayStart());
+    }
+
+    public void Options()
+    {
+        // If player clicks on options button
+        optionsMenu.SetActive(true);
+        MainMenuUI.SetActive(false);
+    }
+
+    public void BackToPause()
+    {
+        // Coming back from options to main menu
+        optionsMenu.SetActive(false);
+        MainMenuUI.SetActive(true);
+    }
+
+    public void QuitPanelOn()
+    {
+        // Asks if player is sure they want to quit the game, also activates UIBlocker to prevent them from being able to click Play, Options, or Quit during this part.
+        QuitYesOrNoUI.SetActive(true);
+        UIBlocker.SetActive(true);
+    }
+
+    // If player clicks YES, confirming to quit, quit game
+    public void Quit()
+    {
+        StartCoroutine(DelayQuit());
+    }
+
+    // If player does not want to quit by clicking NO, deactivate UIBlocker and return back to main menu
+    public void QuitCancel()
+    {
+        QuitYesOrNoUI.SetActive(false);
+        UIBlocker.SetActive(false);
+    }
+
+    private IEnumerator DelayStart()
+    {
+        if (menuMusic != null && fadeTransition != null)
+        {
+            // Prevent player from being able to press buttons during transition to next scene
+            UIBlocker.SetActive(true);
+            // Start both music fading and fade transition at the same time
+            StartCoroutine(menuMusic.FadeOut());
+            StartCoroutine(fadeTransition.FadeInImage());
+
+            // Wait for whichever takes longer, either music fade duration or fade transition
+            yield return new WaitForSecondsRealtime(Mathf.Max(menuMusic.fadeDuration, fadeTransition.fadeDuration));
+        }
+        else
+        {
+            // Only if music and fade is not assigned
+            yield return new WaitForSecondsRealtime(delayTime);
+        }
+
+        // Disable UIBlocker, in case it affects later scenes
+        UIBlocker.SetActive(false);
+        // Load the first level
+        SceneManager.LoadSceneAsync("Map_Design"); // TEMPORARY // TEMPORARY// TEMPORARY// TEMPORARY// TEMPORARY// TEMPORARY// TEMPORARY// TEMPORARY// TEMPORARY// TEMPORARY// TEMPORARY// TEMPORARY
+    }
+
+    private IEnumerator DelayQuit()
+    {
+        // If player is sure to quit, wait a second (mainly to allow click sfx to play)
+        // Quit the game for player if they are either on application or editor
+        yield return new WaitForSecondsRealtime(delayTime);
+        if (UnityEditor.EditorApplication.isPlaying == true)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+}
